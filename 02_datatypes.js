71d0c213e64fb75c3e505594bcf3a3d03db4be50
aryanploxxx <aryangupta8291@gmail.com>
2023-10-04 17:43:57 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"
+// Treat all JS code as newer version of JS - although optional since by default in new browsers it is treated as newer version only
+
+alert("Hello")
+// Works on browser, not on nodejs therefore when running by 'node filenam.js' it gives error
+
+// MDN Docs is not the original documentation - tc39.es is!
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// # Datatypes
+// number -> 2^53
+// binint
+// string -> ""
+// boolean -> true/false
+// null - datatype, also a standalone value - represneation of empty value
+// undefined - when value hasn't been assigned to a defined variable
