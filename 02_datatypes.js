b296bacc2bfb20ec647ce4422e982cc532b05021
aryanploxxx <aryangupta8291@gmail.com>
2023-10-07 00:34:42 +0530

    add info in 02_datatypes.js

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -2,7 +2,9 @@
 // Treat all JS code as newer version of JS - although optional since by default in new browsers it is treated as newer version only
 
 alert("Hello")
+alert(3+3)
 // Works on browser, not on nodejs therefore when running by 'node filenam.js' it gives error
+// But works perfectly fine when run on browser
 
 // MDN Docs is not the original documentation - tc39.es is!
 
@@ -15,5 +17,15 @@ let isLoggedIn = false
 // binint
 // string -> ""
 // boolean -> true/false
-// null - datatype, also a standalone value - represneation of empty value
+// null - datatype, also a standalone value - represneation of empty value - undefined nhi hai, bas khaali hai
 // undefined - when value hasn't been assigned to a defined variable
+// symbols - used when uniqueness is required
+
+console.log(typeof "aryan")
+// String
+console.log(typeof age)
+// Number
+console.log(typeof null)
+// Object - Important to note
+console.log(typeof undefined)
+// undefined
