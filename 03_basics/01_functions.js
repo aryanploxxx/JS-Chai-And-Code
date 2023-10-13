fb20250054c308c26f8ec3ab18fe690ccc32766d
aryanploxxx <aryangupta8291@gmail.com>
2023-10-14 01:04:17 +0530

    functions completed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,17 +14,98 @@ myName()
 
 // In JS we do not need to pass datatypes for parameters
 function addTwoNumber(num1, num2) {
+    console.log(num1+num2);
+}
+addTwoNumber(3,4)
+// NaN
+addTwoNumber(3,"4")
+// 34
+addTwoNumber(3,"a")
+// 3a
+addTwoNumber(3,null)
+// 3
 
+/*
+    Parameters - Values in the function defination
+    Arguements - Values passed when the function is called
+*/
+
+const result = addTwoNumber(3, 5)
+console.log("Result is: ",result);
+// Result is: undefined
+// This happened because there was return type in the function hence it did not return back anything specifically after being called
+
+// To solve this problem, redefine the function
+function addTwoNumber(num1, num2) {
+    let result = num1+num2
+    return result
+    // OR return num1+num2
+}
+// Anything written after return statement will not be executed because function exists after encountering the return function
+
+function loginUserMsg(username) {
+    return `${username} just logged in.`
+}
+console.log(loginUserMsg("Aryan"));
+// Aryan just logged in.
+console.log(loginUserMsg());
+// undefined just logged in. -> To prevent this, we can do something like follows
+
+function loginUserMsg(username) {
+    if(username === undefined) {
+        console.log("Please enter the username");
+        return
+    }
+    return `${username} just logged in.`
 }
 
+// "" - assumed as false value
+// undefined - assumed as false value
+
+// if(!username) -> Ensures this statement runs only when username id undefined, because the '!' make the overall statement in of condition as false
+
+function loginUserMsg(username = "sam") {}
+// by default value of username would be sam
+
+function calculateCartPrice(num1) {
+    return num1
+}
+
+// REST Operator
+function calculateCartPrice(...num1) {
+    return num1
+}
+console.log(calculateCartPrice(200, 100, 3000));
+// [200, 100, 3000] -> num1 will now store all these 3 values as an array
 
 
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+console.log(calculateCartPrice(200, 100, 3000, 10, 50));
+// val1 - 200, val2 - 100
+// num1 - [3000, 10, 50]
 
 
+// Handling objects in functions
+const user = {
+    username: "hitesh",
+    price: 199
+}
 
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+handleObject(user)
+// OR
+handleObject({
+    username: "hitesh",
+    price: 199
+})
 
-
-
-
-
-
+const myArr = [200, 100, 3000, 10]
+function returnSecValue(getarr) {
+    return getarr[1]
+}
+returnSecValue(myArr)
+// 400
