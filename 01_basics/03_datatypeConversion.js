af18d0cdeca1a3b73560917f0bce327112a0f34d
aryanploxxx <aryangupta8291@gmail.com>
2023-12-19 09:45:45 +0530

    Modify Notes

--- 01_basics/03_datatypeConversion.js
+++ 01_basics/03_datatypeConversion.js
@@ -1,58 +1,60 @@
-let score = 33
+let testScore = 33
 
-console.log(typeof score)
-console.log(typeof(score))
+console.log(typeof testScore)
+console.log(typeof(testScore))
 // Both are acceptable methods
+// Will output - number
 
 /*-------------- [ Case 1 ]--------------*/
-let value = Number(score)
-// Convert the datatype of score in number - N in Number method is capital
-console.log(typeof value)
-// number
-console.log(value)
-// 33
-
-/*-------------- [ Case 2 ]--------------*/
-let score = "33abc"
-let value = Number(score)
-console.log(typeof value)
-// number
-console.log(value)
-// NaN
-
-/*-------------- [ Case 3 ]--------------*/
-let score = true
-let value = Number(score)
-console.log(typeof value)
-// number
-console.log(value)
-// 1
-
-/*-------------- [ Case 4 ]--------------*/
-let score1 = null
-let value2 = Number(score1)
-console.log(typeof value2)
-// number
-console.log(value2)
-// 0
-
-/*-------------- [ Case 5 ]--------------*/
-let score = undefined
-let value1 = Number(score)
+let score1 = 33
+let value1 = Number(score1)
+// Convert the datatype of score1 in number - N in Number method is capital
 console.log(typeof value1)
 // number
 console.log(value1)
+// 33
+
+/*-------------- [ Case 2 ]--------------*/
+let score2 = "33abc"
+let value2 = Number(score2)
+console.log(typeof value2)
+// number
+console.log(value2)
+// NaN
+
+/*-------------- [ Case 3 ]--------------*/
+let score3 = true
+let value3 = Number(score3)
+console.log(typeof value3)
+// number
+console.log(value3)
+// 1
+
+/*-------------- [ Case 4 ]--------------*/
+let score4 = null
+let value4 = Number(score4)
+console.log(typeof value4)
+// number
+console.log(value4)
+// 0
+
+/*-------------- [ Case 5 ]--------------*/
+let score5 = undefined
+let value5 = Number(score5)
+console.log(typeof value5)
+// number
+console.log(value5)
 // NaN
 
 /*-------------- [ Case 6 ]--------------*/
-let scoree = "aryan"
-let valueee = Number(scoree)
-console.log(typeof valueee)
+let score6 = "aryan"
+let value6 = Number(score6)
+console.log(typeof value6)
 // number
-console.log(valueee)
+console.log(value6)
 // NaN
 
-
+/*-------------- [ Case 7 ]--------------*/
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(typeof booleanIsLoggedIn)
@@ -78,13 +80,14 @@ console.log(stringToNum)
 const valuee = 3
 let negValue = -valuee
 console.log(negValue);
+// -3
 
 console.log(2+2);
 console.log(2-2);
 console.lo(2*2);
 console.lo(2**3);   // Exponent
-console.log(2/3);
-console.log(2%3);   // Remainder
+console.log(2/3);   // 0.6666666666666666
+console.log(2%3);   // Remainder - 2
 
 
 /*-------- [ Confusing String and Num Conversions ]--------*/
@@ -96,6 +99,8 @@ console.log("1" + 2 + 2)
 // 122
 console.log(l + 2 + "2")
 // 32
+// -> Above 4 have a type of 'string'
+
 console.log(true)
 // true
 console.log(+true)
@@ -104,3 +109,4 @@ console.log(+"")
 // 0 - Conversion happened using +
 
 counter++
+// increment the value of counter by 1
