6a5056de003659431d3389487e1b3a7a7a6f3c1b
aryanploxxx <aryangupta8291@gmail.com>
2023-12-19 09:55:21 +0530

    Modify Notes

--- 01_basics/04_comparsions.js
+++ 01_basics/04_comparsions.js
@@ -6,13 +6,31 @@ console.log(2 != 1)
 
 console.log("2" > 1)
 // true - JS automatically converted string to num
+/*
+    It performs a numerical comparison if both operands are numbers, 
+    and a lexicographical (dictionary order) comparison if one or both operands are strings.
+    In this case, you are comparing a string ("2") with a number (1). 
+    JavaScript will attempt to convert the string to a number before making the comparison. 
+    The string "2" can be converted to the number 2, so the comparison becomes 2 > 1, which is true. 
+    Therefore, the output will be true.
+*/
+
 console.log("02" > 1)
 // true
 
 console.log(null > 0)
-/// false
+//false
+// -> when comparing null with a numeric value using the greater than (>) operator, 
+//    null is treated as 0.
+
 console.log(null == 0)
 // false
+// -> The equality operator (==) in JavaScript performs type coercion. 
+//    However, null is only equal to undefined or itself when using the equality operator.
+
+console.log(null == undefined)
+// true
+
 console.log(null >= 0)
 // true
 
