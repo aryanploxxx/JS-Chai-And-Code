0cd4377f91e46a4b39d164da68b3ac7f3a5b36d0
aryanploxxx <aryangupta8291@gmail.com>
2023-12-31 23:38:31 +0530

    APIs

---
+++ 09_Advance/04_API.js
@@ -0,0 +1,17 @@
+// Application Programming Interface
+/*
+    Public APIs:
+    https://api.github.com/users
+    https://randomuser.me/
+*/
+
+/*
+    Sometimes APIs are very large and difficult to read - Use jsonformatter.org to beutify the api in readable format
+
+    XMLHttpRequest.readyState
+    0	UNSENT	Client has been created. open() not called yet.
+    1	OPENED	open() has been called.
+    2	HEADERS_RECEIVED	send() has been called, and headers and status are available.
+    3	LOADING	Downloading; responseText holds partial data.
+    4	DONE	The operation is complete.
+*/
